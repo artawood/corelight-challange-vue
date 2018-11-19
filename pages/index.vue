60668fee84a2da5cc5eb8fc82cf7576e4e82afac
Artawood Chitamitara <artawood@Artawoods-MacBook-Pro.local>
2018-11-18 21:18:13 -0800

    added cheerio

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
   <section class="container">
     <div>
-      <!-- render components here -->
+      <logo/>
+      <h1 class="title">
+        This is a test
+      </h1>
+      <h2 class="subtitle">
+        corelight blog challenge
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
     </div>
   </section>
 </template>
@@ -10,7 +26,9 @@
 import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {}
+  components: {
+    Logo
+  }
 }
 </script>
 
